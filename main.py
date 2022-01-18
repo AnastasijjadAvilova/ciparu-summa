f156760c84257f70a3796e193b4e0dbb8a5567d5
Anastasia Avilova <77328272+AnastasijjadAvilova@users.noreply.github.com>
2022-01-18 13:43:17 +0000

    "2" aizvieto ar "divi"

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def countZeros(teksts):
-  return teksts.count("0")
+teksts=input("Ievadit tekstu : ")
+def replaceTwos(teksts):
+  if teksts.count("2")>0:
+    teksts =teksts.replace("2","divi")
+    print (teksts)
+  else:
+    teksts = "Nekas netika aizvietots"
+    print (teksts)
+  return teksts
+replaceTwos(teksts)
 
-teksts=input("Ievadit skaitļu : ")
-print(countZeros(teksts))
